130b43f2d99cddf8093f4744757f938e62933d6d
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-07-27 13:44:03 +0600

    [feat]: different colors depending on contribution value]

--- src/components/ContributionItem/ContributionItem.jsx
+++ src/components/ContributionItem/ContributionItem.jsx
@@ -4,10 +4,23 @@ import style from './ContributionItem.module.css';
 export default function ContributionItem({number, date}) {
 
   const [showDetails, setShowDetails] = useState(false);
+  let styleClass;
+  if (number === 0) {
+    styleClass = 'level0';
+  } else if (1 <= number && number <= 9) {
+    styleClass = 'level1';
+  } else if (10 <= number && number <= 19) {
+    styleClass = 'level2';
+  } else if (20 <= number && number <= 29) {
+    styleClass = 'level3';
+  } else {
+    styleClass = 'level4';
+  }
+  
 
   return (
     <div className={style.container}>
-      <div className={style.item} onClick={() => setShowDetails(!showDetails)} />
+      <div className={`${style.item} ${styleClass}`} onClick={() => setShowDetails(!showDetails)} />
       {showDetails &&
         <div className={style.details}>
           <span className={style.number}>{number} contributions</span>
